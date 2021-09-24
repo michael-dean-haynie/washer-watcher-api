2d74a214e4dc48e0ca50ffd330cdb2c63869f2be
Michael Haynie <michael.dean.haynie@gmail.com>
2021-09-23 18:33:49 -0700

    make graph basically show changes from api data

--- views/scripts/script.js
+++ views/scripts/script.js
@@ -1,3 +1,6 @@
+let dataBuffer = [];
+const tickDuration = 1000; // ms
+
 // Create WebSocket connection.
 const socket = new WebSocket('ws://localhost:8080');
 
@@ -9,14 +12,16 @@ socket.addEventListener('open', function (event) {
 // Listen for messages
 socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
+    const data = JSON.parse(event.data);
+    dataBuffer.push(...data.readings.map(reading => reading.magnitude));
 });
 
 
 // Chart
-var n = 40,
+var n = 60,
     random = d3.random.normal(0, .5),
-    data = d3.range(n).map(random),
-    data2 = d3.range(n).map(random);
+    data = d3.range(n).map(() => 0)
+    // data2 = d3.range(n).map(random);
 
 var margin = { top: 20, right: 20, bottom: 20, left: 40 },
     width = 960 - margin.left - margin.right,
@@ -27,7 +32,7 @@ var x = d3.scale.linear()
     .range([0, width]);
 
 var y = d3.scale.linear()
-    .domain([d3.min(data), d3.max(data)])
+    .domain([0, 600])
     .range([height, 0]);
 
 var line = d3.svg.line()
@@ -65,16 +70,22 @@ var path = svg.append("g")
 tick();
 
 function tick() {
+    newValue = dataBuffer.reduce(avgReducer, 0);
+    console.log('newValue', newValue);
+    dataBuffer = [];
+
+
 
     // push a new data point onto the back
-    data.push(random());
+    data.push(newValue);
+    
 
     // redraw the line, and slide it to the left
     path
         .attr("d", line)
         .attr("transform", null)
         .transition()
-        .duration(500)
+        .duration(tickDuration)
         .ease("linear")
         .attr("transform", "translate(" + x(-1) + ",0)")
         .each("end", tick);
@@ -82,4 +93,12 @@ function tick() {
     // pop the old data point off the front
     data.shift();
 
+}
+
+function avgReducer(previousValue, currentValue, currentIndex, array) {
+    let result = previousValue + currentValue;
+    if (currentIndex === array.length - 1) {
+        result = result / array.length;
+    }
+    return result;
 }
